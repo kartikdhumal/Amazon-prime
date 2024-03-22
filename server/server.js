aa36b4f9e861b740599132ab0f87af2a5e095ad5
kartik <kartikdhumal24@gmail.com>
2024-03-23 01:37:12 +0530

    removed options

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const corsConfig = {
   origin: 'http://localhost:5173',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsConfig))
 app.options("", cors(corsConfig))
